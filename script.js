51d6325da25ad99e6cfad0fb8824e918428d3231
Joshua Scurfield <joshcscurfield@gmail.com>
2023-09-27 12:46:34 +0100

    put a div for columns and a div for rows inside html manually and attempted to rewrite the function in the DOM accordingly

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 function createGrid() {
-    let divRows = document.createElement("div");
-    let divColumns = document.createElement("div");
-    divRows.setAttribute("id", "rows");
-    divColumns.setAttribute("id", "columns");
-    let rows = document.getElementById("rows").appendChild(divRows);
-    let columns = document.getElementById("columns").appendChild(divColumns);
+  let rows = document.getElementById("rows");
+  let columns = document.getElementById("columns");
 }
 
 createGrid();
